3026954b7d6ab5b65f6f31dcbd032b5f8777d6a3
Gilles Waeber <moi@gilleswaeber.ch>
2019-09-20 01:36:17 +0200

    Add install option to the build script

--- res/build.php
+++ res/build.php
@@ -11,12 +11,14 @@ abstract class App {
 	static $verbose = false;
 	static $help = false;
 	static $offline = false;
+	static $install = false;
 }
 
 foreach(array_slice($argv, 1) as $arg){
-	if(preg_match("#^(?:[-/][h?]|--help)$#i", $arg)) App::$help = true;
-	elseif(preg_match("#^(?:[-/]v|--verbose)$#i", $arg)) App::$verbose = true;
-	elseif(preg_match("#^(?:[-/]l|--offline|--local)$#i", $arg)) App::$offline = true;
+	if(preg_match("#^(?:[-/][^ ]*[h?]|--help)$#i", $arg)) App::$help = true;
+	elseif(preg_match("#^(?:[-/][^ ]*v|--verbose)$#i", $arg)) App::$verbose = true;
+	elseif(preg_match("#^(?:[-/][^ ]*l|--offline|--local)$#i", $arg)) App::$offline = true;
+	elseif(preg_match("#^(?:[-/][^ ]*i|--install)$#i", $arg)) App::$install = true;
 	else echo "[WARNING] Unrecognized argument ".$arg."\n";
 }
 
@@ -35,3 +37,9 @@ $db = new DataBuilder($dp);
 $db->build();
 
 echo "Done!\n";
+
+if(App::$install){
+	echo "Copying data.js... ";
+	echo copy('data/data.js', '../wwwassets/script/data.js') ? 'success' : 'fail!';
+	echo "\n";
+}
